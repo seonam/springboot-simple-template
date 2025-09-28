3f3eb8c7098366714b699f1ed030f3e5f1ac4680
disdong123 <gksrlfw@gmail.com>
2025-09-28 23:16:16 +0900

    version catalog 테스트

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,17 +16,17 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://maven.pkg.github.com/seonam/version-catalog")
             // jitpack 설정으로 아래 크레덴셜은 필요하지 않음.
-            // FIXME. 여기서는 version-catalog 테스트를 위해 필요
+            // FIXME. 여기서는 version-catalog 테스트를 위해 필요. 파이프라인에서 넣도록 수정.
             credentials {
                 // PAT, github username 을 환경변수 (.zshrc 등)로 저장해야합니다.
-                username = System.getenv("DISDONG_USERNAME")
-                password = System.getenv("DISDONG_TOKEN")
+                username = System.getenv("SEONAM_USERNAME")
+                password = System.getenv("SEONAM_TOKEN")
             }
         }
     }
     versionCatalogs {
         create("libs") {
-            from("kr.disdong:spring-version-catalog:0.0.31")
+            from("kr.seonam:spring-version-catalog:0.0.31")
         }
     }
 }
