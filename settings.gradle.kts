638601e1d6ac83cc49f660382412b73e0de681e5
disdong123 <gksrlfw@gmail.com>
2025-09-28 22:57:53 +0900

    jitpack 설정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,15 +10,18 @@ include(
 
 dependencyResolutionManagement {
     repositories {
+        // FIXME. version-catalog 테스트를 위해 필요. 파이프라인에서 넣도록 수정.
+        mavenLocal()
         maven { url = uri("https://jitpack.io") }
         maven {
             url = uri("https://maven.pkg.github.com/seonam/version-catalog")
             // jitpack 설정으로 아래 크레덴셜은 필요하지 않음.
-            // credentials {
-            //     // PAT, github username 을 환경변수 (.zshrc 등)로 저장해야합니다.
-            //     username = System.getenv("DISDONG_USERNAME")
-            //     password = System.getenv("DISDONG_TOKEN")
-            // }
+            // FIXME. 여기서는 version-catalog 테스트를 위해 필요
+            credentials {
+                // PAT, github username 을 환경변수 (.zshrc 등)로 저장해야합니다.
+                username = System.getenv("DISDONG_USERNAME")
+                password = System.getenv("DISDONG_TOKEN")
+            }
         }
     }
     versionCatalogs {
